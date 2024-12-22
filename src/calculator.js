f5bdc63b8834a57297f1c5fbb27610d4967310df
aman4718 <bholukumar4718@gmail.com>
2024-12-22 12:04:59 +0530

    conflict solve

--- src/calculator.js
+++ src/calculator.js
@@ -73,7 +73,7 @@ const Calculator = () => {
           <div style={styles.display}>
             <input
               type="text"
-              value={input || "0"}
+              value={input}
               readOnly
             />
             <input
